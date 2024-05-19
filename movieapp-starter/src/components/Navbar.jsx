8389e69f35cd92ae7d758ad2aa1477c0073ede32
Bekir Kaplan <bekirkaplannnn@gmail.com>
2024-05-19 18:25:49 +0300

    dark mode

--- movieapp-starter/src/components/Navbar.jsx
+++ movieapp-starter/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
+import UserIcon from "../assets/icons/avatar.png"
+import Switch from './Switch'
 
 
 function classNames(...classes) {
@@ -8,8 +10,13 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+
+const currentUser = {displayName: "Bekir Kaplan"};
+
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <>
+    <Disclosure as="nav" className=" bg-neutral-100 dark:bg-gray-900 py-3 dark:text-white fixed w-full top-0 z-20 text-black">
      
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
@@ -17,11 +24,9 @@ export default function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <Link to={"/"}>
+                  <h1>TigerFLix</h1>
+                  </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -30,8 +35,8 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            
-
+                {currentUser && <h5 className=' m-4'>{currentUser?.displayName}</h5>}
+                <Switch/>
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
@@ -40,8 +45,9 @@ export default function Navbar() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        src={currentUser?.photoURL || UserIcon}
+                        alt="user"
+                        referrerPolicy='no-referrer'
                       />
                     </Menu.Button>
                   </div>
@@ -57,32 +63,32 @@ export default function Navbar() {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          <span
+                            
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
                           >
                             Sign out
-                          </a>
+                          </span>
                         )}
                       </Menu.Item>
                     </Menu.Items>
@@ -95,5 +101,7 @@ export default function Navbar() {
     
       
     </Disclosure>
+    <div className=' h-[70px]'></div>
+    </>
   )
 }
--- movieapp-starter/tailwind.config.js
+++ movieapp-starter/tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
+   darkMode: 'selector',
   //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
     //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
